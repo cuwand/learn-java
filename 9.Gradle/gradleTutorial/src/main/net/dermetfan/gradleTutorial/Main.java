7c533aee96a9c8c25b3b5fc83cb9372e0944f35f
adrian <eekkaaadrian@gmail.com>
2019-10-31 11:16:18 +0700

    gradle tutorial

---
+++ 9.Gradle/gradleTutorial/src/main/net/dermetfan/gradleTutorial/Main.java
@@ -0,0 +1,11 @@
+package main.net.dermetfan.gradleTutorial;
+
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Arrays.deepToString(args));
+	}
+
+}
