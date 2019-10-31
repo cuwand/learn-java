bfec791e05ea7b92f56d18fcdc7f3f030ee99e69
adrian <eekkaaadrian@gmail.com>
2019-10-31 13:52:19 +0700

    gradle dropwizard

--- 9.Gradle/gradleTutorial/src/main/net/dermetfan/gradleTutorial/Main.java
+++ 9.Gradle/gradleTutorial/src/main/net/dermetfan/gradleTutorial/Main.java
@@ -1,11 +1,14 @@
 package main.net.dermetfan.gradleTutorial;
 
 import java.util.*;
+import org.apache.commons.lang3.StringUtils;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(Arrays.deepToString(args));
+		System.out.println(Arrays.toString(args));
+		System.out.println(StringUtils.swapCase(Arrays.toString(args)));
 	}
 
 }
